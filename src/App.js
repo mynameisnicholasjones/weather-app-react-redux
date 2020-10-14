1a2350b03288c05ddb9eed41de6e92b199b0d6f4
Nicholas Jones <mynameisnicholasjones@gmail.com>
2020-10-13 20:57:10 -0400

    Implement React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+// React Router
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        
+      </div>
+    </Router>
   );
 }
 
