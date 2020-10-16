72fc875f3a0e593268a000bd2350a79bf0dc14a0
Nicholas Jones <mynameisnicholasjones@gmail.com>
2020-10-16 12:17:14 -0400

    feat: add Preloader.js component and spinner.gif
    the Preloader.js component uses the spinner.gif to alert the
     user when the app is loading resources.

---
+++ src/components/layout/Preloader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Preloader = () => {
+  return (
+    <div className="loadingTextAndSpinnerContainer">
+      <div className="loadingText">Loading</div>
+      <div className="loadingSpinner"></div>
+    </div>
+  );
+};
+
+export default Preloader;
