4842610eec720468d3a3c4958af0c1a5f05301ec
Nicholas Jones <mynameisnicholasjones@gmail.com>
2020-10-16 13:55:51 -0400

    feat(App.js): add getWeatherForcastData
    this function searches for weather data based on a given location
    ex. 'New York, US'
    
    todo: update function to store the city and country of the weather
    location using component state
    
    todo: update function to store 5 days of weather data in an array
    this data will be used to display the weather data to the user

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 // React Router
 import { BrowserRouter as Router } from 'react-router-dom';
+// Imported helper functions
+import { getAPICallValueForOpenWeatherMapDotOrg } from './helpers/helperFunctions';
 // Imported Components
 import Navbar from './components/layout/Navbar';
 
@@ -9,8 +11,35 @@ function App() {
   // App Component Level StateS
   const [loading, setLoading] = useState(false);
 
-  // TODO: use the useEffect hook to create a function that will automatically retrieve weather data from OpenWeatherMap.org as soon as the app is loaded. This will provide default weather data to display to the user. 
   useEffect(() => {
+
+    // Location to get weather data for.
+    const searchText = 'New York, US';
+
+    // This function searches for weather data based a given location. Ex: 'New York, US'. 
+    // This weather data will be output when the app initially loads in order to give the user default weather data.
+    const getWeatherForcastData = async (searchText) => {
+      setLoading(true);
+
+      // Determine which API call to make
+      const apiCallValue = getAPICallValueForOpenWeatherMapDotOrg(searchText);
+
+      // Make the fetch Get request
+      const res = await fetch(apiCallValue);
+
+      // Format the res into the data
+      const data = await res.json();
+
+      console.log(`The weather data for ${data.city.name}, ${data.city.country} is: ${JSON.stringify(data)}`);
+
+      // TODO: 
+        // 1.) Update function to store the city and country of the weather location using component state.
+        // 2.) Update function to store 5 days of weather data in an array to be used in displaying the weather data to the user.
+
+      setLoading(false);
+    };
+
+    getWeatherForcastData(searchText);
   }, []);
 
   return (
