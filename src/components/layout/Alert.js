65312584d54722148d92905d9a21e2d8fc28043a
Nicholas Jones <mynameisnicholasjones@gmail.com>
2020-10-20 09:53:33 -0400

    fix(Alert.js): check if alert prop is undefined
    This commit adds a check to verify that the alert prop is
    not undefined before attempting to render the Alert component.

--- src/components/layout/Alert.js
+++ src/components/layout/Alert.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Alert = ({ alert }) => {
   return (
-    // If the alert prop is not null, then output a styled div with the alert_text and alert_type
-    alert !== null && (
+    // If the alert prop is not null or undefined, then output a styled div with the alert_text and alert_type
+    alert !== null || 'undefined' (
       <div className={`alert alert-${alert.alertType}`}>
         <i className="fas fa-info-circle" /> {alert.alertText}
       </div>
