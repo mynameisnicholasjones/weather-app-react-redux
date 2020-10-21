693475506bebac43efad88c6a7cae72c886337a4
Nicholas Jones <mynameisnicholasjones@gmail.com>
2020-10-21 13:32:16 -0400

    feat(App.js): update getWeatherForcastData()
    This commit updates the getWeatherForcastData() function to handle
    invalid locations.
    
    If the user attempts to search for a location and the weather API
    does not find it, then getWeatherForcastData() will set an alert to
    inform the user the location was not found.

--- src/App.js
+++ src/App.js
@@ -42,28 +42,36 @@ function App() {
     // Format the res into the data
     const data = await res.json();
 
-    console.log(`The weather data for ${data.city.name}, ${data.city.country} is: ${JSON.stringify(data)}`);
+    // Handle the error if api did not find the city the user searched for.
+    if (data.message === 'city not found') {
+      console.log('city not found');
 
-    setWeatherLocation(`${data.city.name}, ${data.city.country}`);
+      showAlert('Location was not found! Please enter a location.', 'danger');
 
-    // Store 5 days of weather data in an array. This will be used to display either the current-day forcast or the five-day forcast to the user.
-    let fiveDayWeatherForcastTemporaryArray = [];
+    } else {
+      console.log(`The weather data for ${data.city.name}, ${data.city.country} is: ${JSON.stringify(data)}`);
+      
+      setWeatherLocation(`${data.city.name}, ${data.city.country}`);
 
-    data.list.forEach((weatherObject, index) => {
-      // Get the first five weather objects that's index is divisible by 8
-      // IMPORTANT NOTE: The OpenWeatherMap.org API returns 40 items (i.e. index 0 through 39). This means that you will always get only five values that divide into 8 without any remainder (i.e. 0, 8, 16, 24, and 32).
-      // So it does not matter which time of day the database is quieried, you will always get the desired five weather objects.
-      if (index % 8 === 0) {
-        fiveDayWeatherForcastTemporaryArray.push(weatherObject);
-      }
-    });
+      // Store 5 days of weather data in an array. This will be used to display either the current-day forcast or the five-day forcast to the user.
+      let fiveDayWeatherForcastTemporaryArray = [];
 
-    // Console log the five weather objects to verify that the data is being retrieved correctly.
-    fiveDayWeatherForcastTemporaryArray.forEach((weatherObject, index) => {
-      console.log(`Weather Object ${(index + 1)}: ${JSON.stringify(weatherObject)}`);
-    })
+      data.list.forEach((weatherObject, index) => {
+        // Get the first five weather objects that's index is divisible by 8
+        // IMPORTANT NOTE: The OpenWeatherMap.org API returns 40 items (i.e. index 0 through 39). This means that you will always get only five values that divide into 8 without any remainder (i.e. 0, 8, 16, 24, and 32).
+        // So it does not matter which time of day the database is quieried, you will always get the desired five weather objects.
+        if (index % 8 === 0) {
+          fiveDayWeatherForcastTemporaryArray.push(weatherObject);
+        }
+      });
 
-    setFiveDayWeatherForcastArray(fiveDayWeatherForcastTemporaryArray);
+      // Console log the five weather objects to verify that the data is being retrieved correctly.
+      fiveDayWeatherForcastTemporaryArray.forEach((weatherObject, index) => {
+        console.log(`Weather Object ${(index + 1)}: ${JSON.stringify(weatherObject)}`);
+      })
+
+      setFiveDayWeatherForcastArray(fiveDayWeatherForcastTemporaryArray);
+    }
 
     setLoading(false);
   };
