880f755d6b192b44cd3d98ed014c231daa71d46d
Nicholas Jones <mynameisnicholasjones@gmail.com>
2020-10-22 19:39:12 -0400

    feat(weatherForecasts): add FiveDayForecast.js
    This commit adds the FiveDayForecast.js component, which is used to
    output five (5) ForecastItem.js components. Each of the five (5)
    ForecastItem.js components corresponds to one day of
    the five day forecast.

---
+++ src/components/weatherForecasts/FiveDayForecast.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// Imports for Components
+import Preloader from '../layout/Preloader';
+import ForecastItem from './ForecastItem';
+
+const FiveDayForecast = ({
+  fiveDayWeatherForecastArray,
+  loading,
+}) => {
+  // The user has not search for anything yet, so singleDayWeatherForecastObject is 'null' and there is nothing to show.
+  if (fiveDayWeatherForecastArray === null) {
+    return null;
+  } else if (loading) {
+    return <Preloader />;
+  } else {
+    return (
+      <div>
+        {/* NOTE: The OpenWeatherMap.org API did not include a 'city: id' for the Five Day Forecast, which means that I have to use the 'last resort' of using the array index of each singleDayWeatherForecastObject as the key value */}
+        {fiveDayWeatherForecastArray.map(
+          (singleDayWeatherForecastObject, index) => (
+            <ForecastItem
+              singleDayWeatherForecastObject={singleDayWeatherForecastObject}
+              // NOTE: There are no stable IDs for rendered items, so you may use the item index as a key as a last resort:
+              key={index}
+            />
+          )
+        )}
+      </div>
+    );
+  }
+};
+
+FiveDayForecast.propTypes = {
+  fiveDayWeatherForecastArray: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+}
+
+export default FiveDayForecast;
