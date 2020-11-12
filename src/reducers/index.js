d7c0326e9928e05380d52f00abc6ba1ca86cb209
Nicholas Jones <mynameisnicholasjones@gmail.com>
2020-11-12 17:55:42 -0500

    feat(index.js): add forecastReducer
    This commit adds the forecastReducer into combineReducers.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 // NOTE: index.js is the Root Reducer
 import { combineReducers } from 'redux';
 import alertReducer from './alertReducer';
+import forecastReducer from './forecastReducer';
 
 // NOTE: combineReducers takes in an object that contains all of your reducers
 export default combineReducers({
   // [The name for the reducer's state]: [The name of the corresponding reducer]
   alertReducerState: alertReducer,
+  forecastReducerState: forecastReducer,
 });
