fd3beb7609c52f99e3e33d8a8ce999e967e9cb36
Nicholas Jones <mynameisnicholasjones@gmail.com>
2020-11-12 14:28:03 -0500

    feat(reducers): add alertReducers.js
    This commit adds the alertReducers.js file which will be used to
    contain all of the reducers used by the Alert component.
    
    NOTE: Reducers are functions that take the current state and an action
    as arguments, and return a new state result. In other words,
    (state, action) => newState.

---
+++ src/reducers/alertReducer.js
@@ -0,0 +1,15 @@
+import {  } from '../actions/types';
+
+// Create the initial state of the forcastsReducer
+const initialState = {
+  // NOTE: Data type is: {}
+  alert: null,
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+
+    default:
+      return state;
+  }
+};
