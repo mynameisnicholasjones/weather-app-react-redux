c6d4687112e3ee3e0bf7767491b3d6b457eb7cf1
Nicholas Jones <mynameisnicholasjones@gmail.com>
2020-11-12 14:33:46 -0500

    feat(index.js): add alertReducer
    This commit adds the alertReducer into combineReducers.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,9 @@
 // NOTE: index.js is the Root Reducer
 import { combineReducers } from 'redux';
+import alertReducer from './alertReducer';
 
 // NOTE: combineReducers takes in an object that contains all of your reducers
-export default combineReducers({});
+export default combineReducers({
+  // [The name for the reducer's state]: [The name of the corresponding reducer]
+  alertReducerState: alertReducer,
+});
