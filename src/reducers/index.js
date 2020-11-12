bada85bb19a8b4969f9d032d135a5619258ee0d8
Nicholas Jones <mynameisnicholasjones@gmail.com>
2020-11-12 11:47:57 -0500

    feat(src): add root reducer 'index.js'
    This commit adds the 'reducers' folder and the root reducer 'index.js'.
    
    The root reducer will be used to combine all other reducers for the
    weather app.

---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+// NOTE: index.js is the Root Reducer
+import { combineReducers } from 'redux';
+
+// NOTE: combineReducers takes in an object that contains all of your reducers
+export default combineReducers({});
