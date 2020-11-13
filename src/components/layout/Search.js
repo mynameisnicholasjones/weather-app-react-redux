c019ce9115729e10025ee3567a4ad6e6e38eefe2
Nicholas Jones <mynameisnicholasjones@gmail.com>
2020-11-12 23:03:45 -0500

    refactor(Search.js): use setAlertAction
    This commit refactors the Search.js component to use the setAlertAction
    from global state instead of showAlert which uses component state.

--- src/components/layout/Search.js
+++ src/components/layout/Search.js
@@ -3,8 +3,9 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 // NOTE: Whenever you call an action from a component, you have to import that action
 import { getWeatherForecastAction } from '../../actions/forecastActions';
+import { setAlertAction } from '../../actions/alertActions';
 
-const Search = ({ getWeatherForecastAction, showAlert }) => {
+const Search = ({ getWeatherForecastAction, setAlertAction }) => {
   // Component level state used in search input form.
   const [searchText, setSearchText] = useState('');
 
@@ -19,7 +20,7 @@ const Search = ({ getWeatherForecastAction, showAlert }) => {
 
     if (searchText === '') {
       // Show alert
-      showAlert('Please enter a location', 'danger');
+      setAlertAction('Please enter a location', 'danger');
     } else {
       // Get the weather forecast
       getWeatherForecastAction(searchText);
@@ -55,4 +56,4 @@ Search.propTypes = {
 
 // NOTE: Since mapStateToProps is not used in this component, the
 // first arguement for connect is null.
-export default connect(null, { getWeatherForecastAction })(Search);
+export default connect(null, { getWeatherForecastAction, setAlertAction })(Search);
