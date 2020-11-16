d1cdc3fd6a71cd6b4952bee3c643aaceddf87e55
Nicholas Jones <mynameisnicholasjones@gmail.com>
2020-11-16 14:51:59 -0500

    fix(Navbar.js): add missing import
    This commit adds a missing import for PropTypes

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 // NOTE: Whenever you call an action from a component, you have to import that action
 import { getShowFiveDayForecastFlagAction } from '../../actions/forecastActions';
