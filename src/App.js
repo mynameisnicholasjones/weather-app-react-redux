d5f9b2fb255350899957e0101b0605354bc1a1b2
Nicholas Jones <mynameisnicholasjones@gmail.com>
2020-11-16 21:14:27 -0500

    refactor(App.js): delete getWeatherForecastData
    This commit deletes the function declaration for getWeatherForecastData.
    
    This was done because getWeatherForecastData was replaced with
    getWeatherForecastAction located in forecastActions.js
    
    TODO: Delete the following component level state that was previously
    set in getWeatherForecastData, since these state values are now stored
    and set using global state:
    
      1.) loading
      2.) weatherLocation
      3.) fiveDayWeatherForecastArray

--- src/App.js
+++ src/App.js
@@ -42,53 +42,6 @@ function App() {
     setUserClickedWeatherForecastObject(singleDayWeatherForecastObject);
   };
 
-  // This function searches for weather data based a given location. Ex: 'New York, US'.
-  const getWeatherForecastData = async (searchText) => {
-    setLoading(true);
-
-    // Determine which API call to make
-    const apiCallValue = getAPICallValueForOpenWeatherMapDotOrg(searchText);
-
-    // Make the fetch Get request
-    const res = await fetch(apiCallValue);
-
-    // Format the res into the data
-    const data = await res.json();
-
-    // Handle the error if api did not find the city the user searched for.
-    if (data.message === 'city not found') {
-      console.log('city not found');
-
-      // showAlert(`Unable to find location: "${searchText}". Please try again.`, 'danger');
-
-    } else {
-      console.log(`The weather data for ${data.city.name}, ${data.city.country} is: ${JSON.stringify(data)}`);
-
-      setWeatherLocation(`${data.city.name}, ${data.city.country}`);
-
-      // Store 5 days of weather data in an array. This will be used to display either the current-day forecast or the five-day forecast to the user.
-      let fiveDayWeatherForecastTemporaryArray = [];
-
-      data.list.forEach((weatherObject, index) => {
-        // Get the first five weather objects that's index is divisible by 8
-        // IMPORTANT NOTE: The OpenWeatherMap.org API returns 40 items (i.e. index 0 through 39). This means that you will always get only five values that divide into 8 without any remainder (i.e. 0, 8, 16, 24, and 32).
-        // So it does not matter which time of day the database is quieried, you will always get the desired five weather objects.
-        if (index % 8 === 0) {
-          fiveDayWeatherForecastTemporaryArray.push(weatherObject);
-        }
-      });
-
-      // Console log the five weather objects to verify that the data is being retrieved correctly.
-      fiveDayWeatherForecastTemporaryArray.forEach((weatherObject, index) => {
-        console.log(`Weather Object ${(index + 1)}: ${JSON.stringify(weatherObject)}`);
-      })
-
-      setFiveDayWeatherForecastArray(fiveDayWeatherForecastTemporaryArray);
-    }
-
-    setLoading(false);
-  };
-
   return (
     <Provider store={store}>
       <Router>
