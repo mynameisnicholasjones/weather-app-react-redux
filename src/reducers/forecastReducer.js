99a1ece7f2152cde326c74bf01264a8fb6ab0256
Nicholas Jones <mynameisnicholasjones@gmail.com>
2020-11-16 19:30:09 -0500

    fix(forecastReducer.js): fix typo in comment
    This commit fixes a typo in a comment that refers to an instance of
    the state 'fiveDayWeatherForcastArray'. The instance of the state is
    now spelled correctly with an 'e' as in 'fiveDayWeatherForecastArray'.

--- src/reducers/forecastReducer.js
+++ src/reducers/forecastReducer.js
@@ -41,7 +41,7 @@ export default (state = initialState, action) => {
     case GET_WEATHER_FORECAST:
       return {
         ...state,
-        // payload: fiveDayWeatherForcastArray,
+        // payload: fiveDayWeatherForecastArray,
         fiveDayWeatherForecastArray: action.payload,
         loading: false,
       }
