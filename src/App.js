7b3d8c56e07d577a3e5f2f3783fde2fa0bf0292d
Nicholas Jones <mynameisnicholasjones@gmail.com>
2020-11-21 13:21:45 -0500

    refactor(App.js): delete import of useState
    This commit refactors the ForecastItem.js component by
    deleting the import of the useState hook.
    
    This was done because the useState hook is no longer used in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 // React Router
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
