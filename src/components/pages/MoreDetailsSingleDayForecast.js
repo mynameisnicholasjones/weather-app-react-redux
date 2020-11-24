de6f7671b3f7a06df816ba02afbf7860749a24ef
Nicholas Jones <mynameisnicholasjones@gmail.com>
2020-11-24 15:45:46 -0500

    refactor(MoreDetailsSingleDayForecast.js): refactor css
    This commit refactors the MoreDetailsSingleDayForecast.js component
    to use css classes instead of inline css.

--- src/components/pages/MoreDetailsSingleDayForecast.js
+++ src/components/pages/MoreDetailsSingleDayForecast.js
@@ -25,61 +25,33 @@ const MoreDetailsSingleDayForecast = ({
   } else {
     return (
       <div className="card">
-        <div style={{ display: 'flex' }}>
-          <div
-            className="text-light"
-            style={{
-              flex: '1',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              fontSize: 'clamp(1.5rem, 6vw, 6rem)',
-            }}
-          >
+
+        <div className="moreDetailsSingleDayForecastContainer">
+
+          <div className="dayOfTheWeek text-light">
             {/* Moment taking in a Unix Timestamp from the OpenWeatherMap.org API and formating the output to show the day of the week as a three letter day (ex. 'Sat') */}
             <Moment unix format="ddd">
               {userClickedWeatherForecastObject['dt']}
             </Moment>
           </div>
 
-          <div
-            style={{
-              flex: '1',
-              paddingRight: '1rem',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-          >
+          <div className="weatherImage">
             {getWeatherImage(userClickedWeatherForecastObject.weather[0].main)}
           </div>
 
-          <div
-            className="text-light"
-            style={{
-              flex: '1',
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-          >
-            <div style={{ fontSize: 'clamp(1rem, 4vw, 2.2rem)' }}>
+          <div className="weatherDetails text-light">
+            <div className="weatherDetails__weatherDescription">
               {/* Weather Description:{' '} */}
               {userClickedWeatherForecastObject.weather[0].main}
             </div>
-            <div style={{ fontSize: 'clamp(1rem, 4vw, 2.2rem)' }}>
+            <div className="weatherDetails__weatherTemp">
               {/* Tempurature:{' '} */}
               {Math.round(userClickedWeatherForecastObject.main.temp)} °F
             </div>
 
             {/* Back Button */}
             <button
-              className="btn btn-dark btn-sm"
-              style={{
-                fontSize: 'clamp(0.7rem, 2vw, 1.5rem)',
-                margin: '0.7em',
-              }}
+              className="weatherBack__detailsBtn btn btn-dark btn-sm"
               onClick={onClickGoToPreviousPage}
             >
               Back
@@ -87,19 +59,7 @@ const MoreDetailsSingleDayForecast = ({
           </div>
         </div>
 
-        <div
-          className="text-light"
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderTop: 'solid 1px #ccc',
-            marginTop: '1rem',
-            paddingTop: '1rem',
-            fontSize: 'clamp(1rem, 4vw, 2.2rem)',
-          }}
-        >
+        <div className="moreWeatherDetails text-light">
           <div>
             Feels Like:{' '}
             {Math.round(userClickedWeatherForecastObject.main.feels_like)} °F
@@ -118,6 +78,7 @@ const MoreDetailsSingleDayForecast = ({
             Pressure: {userClickedWeatherForecastObject.main.pressure} hPa
           </div>
         </div>
+
       </div>
     );
   }
